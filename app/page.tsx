87f0bb2049135b969584e5de76178fbf325db3eb
Emmanuel Okon <emmaudo27@gmail.com>
2024-07-06 20:34:45 +0100

    tailwind config, utils and global css files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-red-600 text-blue-700" >Home</h1>
+      <h1 className="font-bold text-white">Home</h1>
+      <Button> hey</Button>
     </div>
   );
 }
