0cf8e18704f8b3c38227f480a81c1e5a6e21c5d1
Emmanuel Okon <emmaudo27@gmail.com>
2024-09-08 09:49:25 +0100

    created passkey for admin login

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,29 @@
-import PatientForm from "@/components/forms/PatientForm";
-
 import Image from "next/image";
 import Link from "next/link";
 
+import PatientForm from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
+
 const currentYear = new Date().getFullYear();
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
-    <div className="flex justifybetween items-center h-screen max-h-screen">
-      {/* OTP VERIFICATION | PASSKEY MODAL */}
+    <div className="flex items-center h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar overflow-y-auto w-full container flex flex-col max-h-screen">
         <div className="sub-container max-w-[496px] flex-1 justify-between py-10">
-          <Image
-            src="/assets/icons/logo-full.svg"
-            height={1000}
-            width={1000}
-            alt="patient"
-            className="mb-12 h-10 w-fit"
-          />
+          <Link href="/">
+            <Image
+              src="/assets/icons/logo-full.svg"
+              height={1000}
+              width={1000}
+              alt="patient"
+              className="mb-12 h-10 w-fit"
+            />
+          </Link>
 
           <PatientForm />
 
